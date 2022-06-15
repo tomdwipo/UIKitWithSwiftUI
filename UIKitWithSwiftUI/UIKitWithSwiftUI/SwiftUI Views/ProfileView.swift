6df0894f70eb1c5d52b4572d0acdf9de62f681b0
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2022-06-15 16:26:36 +0700

    Walkthrough - Presenting a SwiftUI view with code

---
+++ UIKitWithSwiftUI/UIKitWithSwiftUI/SwiftUI Views/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  UIKitWithSwiftUI
+//
+//  Created by Tommy on 15/06/22.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+            .font(.largeTitle)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
